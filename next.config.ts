699071b2e11b1121951d0a934fe39694b8f37be8
lovemishra28 <lovemishra1606@gmail.com>
2026-03-26 18:42:19 +0530

    Update API destination to use production URL

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,8 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*",
+        // destination: "http://localhost:5000/api/:path*",
+        destination: "https://samvad-backend-p18q.onrender.com/api/:path*",
       },
     ];
   },
